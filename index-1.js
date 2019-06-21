a569540f24ab83bb88c18f4b50a87e192af57ac3
IT-master-code <52080284+IT-master-code@users.noreply.github.com>
2019-06-21 18:20:54 +0200

    The first project
    The first project

---
+++ index-1.js
@@ -0,0 +1,59 @@
+var firstVariable = 10;
+
+console.log("number");
+console.log("The number is: " + firstVariable + " hello");
+
+var number = 10; //number ili INTEGER
+var string = "Hello"; // string
+var float = 10.0; // float ili double
+var boolean = false; // boolean : false ili true
+var object = {}; // objects
+var array = [1, 2, 3, "Hello"]; //array
+var undefined; //undefined
+var empty = null; //null
+var char = "a";
+
+/* 
+lfjsdljfdslkjfsd
+flsjdfldskjfsd
+flks;jf;lkdsj
+*/
+
+var name = "1";
+var Name = 1;
+
+
+
+var test; //declaration
+test = 10; //initialization
+console.log(test);
+
+
+var a = 10;
+var b = 20;
+var c = a + b;
+
+a++; // a = a + 1
+a--; // a = a - 1
+
+a = a + 1;
+a += 1;
+a *= 1;// a = a *1;
+
+console.log(name != Name);
+console.log(name === Name);
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+
+
+var classes = 1;
+var session = 3 * classes;
+var month = 12 * session;
+var result = 5 * month + month/2;
+console.log(result);
+
+
+
+
